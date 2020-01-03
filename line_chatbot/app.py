6a3ee6d632baf16d2b0435261bbff400958cb4dd
Chloe1997 <42937407+Chloe1997@users.noreply.github.com>
2020-01-03 10:20:05 +0800

    Update app.py

--- line_chatbot/app.py
+++ line_chatbot/app.py
@@ -6,14 +6,16 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,StickerMessage,StickerSendMessage
-)
+from linebot.models import *
+
 import requests.packages.urllib3
+
 requests.packages.urllib3.disable_warnings()
 
-
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
 
 line_bot_api = LineBotApi('rIxk6stOI6lawIZHAP8D5JNzvH09Rc4XTddGEUZJ327Npj+Rddp1gRVXAdAo/nONSZtr+t95qu2iZJ/MHuKpJy/qtT9ddV3SMVwaYVkz5onz76vrTIE3yy1YLxZ1meS5K9lmrbpBgDXZNr5aXbFsLAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('d6bf376091a8e30931e9545e8b2908bc')
@@ -22,6 +24,7 @@ handler = WebhookHandler('d6bf376091a8e30931e9545e8b2908bc')
 @app.route("/")
 def home():
     return 'home OK'
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -42,14 +45,17 @@ def callback():
 
     return 'OK'
 
+
 def air():
     r = requests.get("https://opendata.epa.gov.tw/ws/Data/AQI/?$format=json", verify=False)
     list_of_dicts = r.json()
     return list_of_dicts
 
-#處理訊息
+
+# 處理訊息
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
+    # 被動 Reply Message 使用 replyToken 這個 key 裡面的資訊來回復訊息
     line_bot_api.reply_message(
         event.reply_token,
         StickerSendMessage(
@@ -57,33 +63,102 @@ def handle_sticker_message(event):
             sticker_id=event.message.sticker_id)
     )
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     data = air()
-    k=0
+    # message = TextSendMessage(text="請輸入'空氣'來得知訊息"+event.message.text)
+
+    k = 0
     try:
+        if event.message.text == "今天":
+            message = TemplateSendMessage(
+                alt_text='Buttons template',
+                template=ButtonsTemplate(
+                    thumbnail_image_url='https://upload.cc/i1/2020/01/02/psRVCD.jpg',
+                    title='功能選單',
+                    text='Please select',
+                    actions=[
+                        # PostbackTemplateAction是含有值的訊息回覆
+                        PostbackTemplateAction(
+                            label='空氣品質查詢',
+                            text='空氣品質查詢',
+                            # data='action=buy&itemid=1'
+                            data='postback1'
+                        ),
+
+                        # ),
+                        # URITemplateAction是網址的使用
+                        URITemplateAction(
+                            label='前往中央氣象局',
+                            uri='https://www.cwb.gov.tw/V8/C/W/W50_index.html'
+                        )
+                    ]
+                )
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+            TextSendMessage(text=message)
+
         if event.message.text == "空氣":
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text="請輸入位置"))
+
+        if event.message.text == "空氣品質查詢":
+            message = TemplateSendMessage(
+                alt_text='Carousel template',
+                template=CarouselTemplate(
+                    columns=[
+                        CarouselColumn(
+                            thumbnail_image_url='https://upload.cc/i1/2020/01/03/17Pspz.jpg',
+                            title='高雄市',
+                            text='description1',
+                            actions=[
+                                MessageTemplateAction(
+                                    label='高雄(左營)',
+                                    text='高雄(左營)',
+                                ),
+                                MessageTemplateAction(
+                                    label='高雄(楠梓)',
+                                    text='高雄(楠梓)',
+                                )
+                            ]
+                        ),
+                        CarouselColumn(
+                            thumbnail_image_url='https://upload.cc/i1/2020/01/03/ZskDji.jpg',
+                            title='台北市',
+                            text='description2',
+                            actions=[
+                                MessageTemplateAction(
+                                    label='萬華',
+                                    text='萬華',
+                                ),
+                                MessageTemplateAction(
+                                    label='大同',
+                                    text='大同',
+                                )
+
+                            ]
+                        )
+                    ]
+                )
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+
         else:
             for i in data:
                 value = [i["County"], i["SiteName"], i["Status"]]
-                if event.message.text == i["SiteName"] :
-                    k = k+1
+                # if event.message.text == value[0]:
+                if event.message.text == i["SiteName"]:
+                    k = k + 1
                     line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text="您所查詢的空氣品質"+value[2]))
+                    event.reply_token,
+                    TextSendMessage(text="您所查詢的空氣品質" + value[2]))
             if k == 0:
                 line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text="請重新輸入"))
-    except :
+    except:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="發生錯誤"))
-
-
-if __name__ == "__main__":
-    app.run()
-
